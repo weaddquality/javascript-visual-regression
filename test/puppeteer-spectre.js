426d5165d49366a18dd992f7b9992c645f395183
Stefan Jansson <s.jansson@gmail.com>
2018-02-15 12:50:37 +0100

    Remove spectre-support and puppeteer-standalone test from branch

--- test/puppeteer-spectre.js
+++
@@ -1,9 +0,0 @@
-const spectre = require('../utils/spectre');
-const puppeteer = require('../utils/puppeteer');
-
-describe('VRT Example with Puppeteer and Spectre', function() {
-  it('should take a screenshot of the footer logo and post screenshots to spectre', async function() {
-    await puppeteer.takeScreenshot('https://www.spotify.com/se/', '.footer-logo a', 'footerLogoWithMocha', 1280, 1024, false);
-    await spectre.postScreenshots('Spotify', 'Logo', 'footerLogoWithMocha', '1280');
-  });
-});
