98c05c57249bab55f7802f4bec4bfe9627f71620
Stefan Jansson <s.jansson@gmail.com>
2018-02-27 01:25:17 +0100

    Refactor error message of  detecting a difference above set fuzzlevel

--- test/puppeteer-spectre.js
+++ test/puppeteer-spectre.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 let settings = {
   url: 'https://www.spotify.com/se/',
-  element: '.footer-logo a',  
+  element: '.footer-logo',  
   testName: 'Footer logo',
   width: 1280,
   height: 1024,
@@ -17,6 +17,6 @@ describe('VRT Example with Puppeteer and Spectre', function() {
   it('should take a screenshot of the footer logo and post screenshots to spectre', async function() {
     await puppeteer.takeScreenshot(settings);
     const result = await spectre.postScreenshot(settings);
-    assert(result.pass, 'A diff between the screenshots has been detected');
+    assert(result.pass, 'A differance above the set fuzzlevel ' + settings.fuzzLevel + ' has been detected');
   });
 });
