375afb88e243e67085c90b1a669860e8aef5cfd4
Stefan Jansson <s.jansson@gmail.com>
2018-03-04 00:16:06 +0100

    Refactor to put hide and remove-functionality of elements as utils

--- utils/puppeteer.js
+++ utils/puppeteer.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const conf = require('../conf');
+const elements = require('./elements');
 
 exports.takeScreenshot = async function(settings) {
   let browser;
@@ -14,22 +15,8 @@ exports.takeScreenshot = async function(settings) {
     height: settings.height
   });
   await page.goto(settings.url, { waitUntil: 'networkidle2' });
-  if (!(settings.hideElements === undefined) && !(settings.hideElements.length === 0)) {
-    await Promise.all(
-      settings.hideElements.map(function(element) {
-        page.evaluate(function(element) {
-          document.querySelector(element).style.setProperty('visibility', 'hidden');
-        }, element);
-      }));
-  }
-  if (!(settings.removeElements === undefined) && !(settings.removeElements.length === 0)) {
-    await Promise.all(
-      settings.removeElements.map(function(element) {
-        page.evaluate(function(element) {
-          document.querySelector(element).style.setProperty('display', 'none');
-        }, element);
-      }));
-  }
+  await elements.hide(settings, page);
+  await elements.remove(settings, page);
   const element = await page.$(settings.element);
   settings.snapshot = await element.screenshot();
   await browser.close();
