b3949f29eaa39af7deab836533e67b80b91dec1c
Måns Nilsson <mans.nilsson@thomascook.se>
2018-03-17 00:33:06 +0100

    Update message for failed test

--- examples/puppeteer-spectre.js
+++ examples/puppeteer-spectre.js
@@ -19,6 +19,6 @@ describe('VRT Example with Puppeteer and Spectre', function() {
   it('should take a screenshot of the footer logo and post screenshots to spectre', async function() {
     await puppeteer.takeScreenshot(settings);
     const result = await spectre.postScreenshot(settings);
-    await asserts.noDifferance(result, settings);
+    await asserts.noDifference(result, settings);
   });
 });
--- utils/asserts.js
+++ utils/asserts.js
@@ -1,5 +1,7 @@
 const assert = require('assert');
 
-exports.noDifferance = async function(result, settings) {
-  await assert(result.pass, 'A differance above the set fuzzlevel ' + settings.fuzzLevel + ' has been detected');
+exports.noDifference = async function(result, settings) {
+  await assert(result.pass, `Mismatch detected.
+                                      Difference was: ${result.diff}%.
+                                      Accepted fuzzlevel: ${settings.fuzzLevel}.`);
 };
